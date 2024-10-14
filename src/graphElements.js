64a1028e9548e7d6310135d1e646797ffd9463fe
Jonathan Cook <jonathancook516@gmail.com>
2024-10-14 17:15:43 -0400

    more complex hardcoded graph

--- src/graphElements.js
+++ src/graphElements.js
@@ -1,26 +1,65 @@
 import React from 'react';
 
+const position = { x: 0, y: 0 };
+
 export const initialNodes = [
   {
     id: '1',
     type: 'input',
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 25 },
+    data: { label: 'input' },
   },
   {
     id: '2',
-    data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
+    data: { label: 'node 2 ' },
+  },
+  {
+    id: '2a',
+    data: { label: 'node 2a ' },
+  },
+  {
+    id: '2b',
+    data: { label: 'node 2b ' },
+  },
+  {
+    id: '2c',
+    data: { label: 'node 2c ' },
+  },
+  {
+    id: '2d',
+    data: { label: 'node 2d ' },
   },
   {
     id: '3',
+    data: { label: 'node 3 ' },
+  },
+  {
+    id: '4',
+    data: { label: 'node 4 ' },
+  },
+  {
+    id: '5',
+    data: { label: 'node 5 ' },
+  },
+  {
+    id: '6',
     type: 'output',
-    data: { label: 'Output Node' },
-    position: { x: 250, y: 250 },
+    data: { label: 'output' },
+  },
+  {
+    id: '7',
+    type: 'output',
+    data: { label: 'output' },
   },
 ];
 
 export const initialEdges = [
-  { id: 'e1-2', source: '1', target: '2' },
-  { id: 'e2-3', source: '2', target: '3', animated: true },
+  { id: 'e12', source: '1', target: '2', animated: true},
+  { id: 'e13', source: '1', target: '3', animated: true },
+  { id: 'e22a', source: '2', target: '2a', animated: true },
+  { id: 'e22b', source: '2', target: '2b', animated: true },
+  { id: 'e22c', source: '2', target: '2c', animated: true },
+  { id: 'e2c2d', source: '2c', target: '2d', animated: true },
+  { id: 'e45', source: '4', target: '5', animated: true },
+  { id: 'e56', source: '5', target: '6', animated: true },
+  { id: 'e57', source: '5', target: '7', animated: true },
 ];
