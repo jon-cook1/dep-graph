4a9a02c0dee11d2180b4afce4196728ec223dea8
Jonathan Cook <jonathancook516@gmail.com>
2024-10-16 10:06:45 -0400

    allow code to persist on page refresh

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AceEditor from 'react-ace';
 
 import 'ace-builds/src-noconflict/mode-python';
 import 'ace-builds/src-noconflict/theme-monokai';
 
 const CodeEditor = ({ code, setCode }) => {
+  
+  // Load code from localStorage when the component mounts
+  useEffect(() => {
+    const savedCode = localStorage.getItem('student_code');
+    if (savedCode) {
+      setCode(savedCode);
+    }
+  }, [setCode]);
+
+  // Save code to localStorage
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    localStorage.setItem('student_code', newCode);
+  };
+
   return (
     <div className="code-editor">
       <AceEditor
         mode="python"
         theme="monokai"
         name="code_editor"
-        onChange={setCode}
+        onChange={handleCodeChange}
         value={code}
         width="100%"
         height="100%"
